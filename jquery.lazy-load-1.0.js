85d7398029894bc11550fc73e61deffde5bcb5e1
gserrano <contato@guilhermeserrano.com.br>
2012-07-09 20:22:50 -0300

    Demo e link para documentacao

--- jquery.lazy-load-1.0.js
+++ jquery.lazy-load-1.0.js
@@ -1,13 +1,12 @@
 /*
- * jQuery LazyLoad Plugin v1.0
+ * jQuery Lazy Load Plugin v1.0
+ * http://andafter.org/labs/lazyload
  * https://github.com/gserrano/jQuery-Lazy-Load
- * http://odesenvolvedor.com.br/
  *
  * Copyright 2011, Guilherme Serrano - http://guilhermeserrano.com.br
  * Dual licensed under the MIT or GPL Version 2 licenses.
  *
- * Required: jQuery Javascript Library
- * http://jquery.com/
+ * Required: jQuery
  */
 
 (function($){
